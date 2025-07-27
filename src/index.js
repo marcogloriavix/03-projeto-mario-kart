be573a34785923c6e8583a187d91df14ed616072
marcogloriavix <marcogloriavix@gmail.com>
2025-07-27 12:05:39 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const player1 = {
+    Nome: "Mario",
+    Velocidade: 4,
+    Manobralidade: 3,
+    Poder: 3,
+    Pontos: 0,
+
+};
+
+const player2 = {
+    Nome: "Luigi",
+    Velocidade: 3,
+    Manobralidade: 4,
+    Poder: 4,
+    Pontos: 0,
+
+};
+
